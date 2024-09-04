a247fd559acb0276c3ea2c56a048e8ff939923a0
五月雨 <125344400+sam1dare-kbd@users.noreply.github.com>
2024-09-04 17:58:14 +0900

    Delete grain9 directory

--- grain9/firmware/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0xD2, 0xA6, 0x11, 0x1D, 0x11, 0xCA, 0xD5, 0xA3}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
